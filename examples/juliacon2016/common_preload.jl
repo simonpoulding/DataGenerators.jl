570e92fd5ddb47a1a75296a257d84d868df92620
Robert Feldt <robert.feldt@gmail.com>
2016-06-22 10:27:20 -0400

    common preload file for juliacon repl session (ensures things are included and compiled to reduce delays later)

---
+++ examples/juliacon2016/common_preload.jl
@@ -0,0 +1,16 @@
+using DataGenerators
+using DataMutators
+
+# To ensure things are compiled let's define simple generator and use it
+@generator SomeData begin
+    start() = [element() for i in 1:rand(2:3)]
+    element() = choose(ASCIIString, "\\w+\\.\\w+")
+    element() = choose(Int, 1, 100)
+    element() = start()
+end
+gen = SomeData();
+big = choose(gen);
+
+prop(l) = length(l) < 2
+
+smaller = DataMutators.shrink(big, prop);
