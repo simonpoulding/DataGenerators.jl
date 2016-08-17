837e26eed288f6de382a574f70ad61c000933251
Simon Poulding <simon.poulding@gmail.com>
2016-08-17 03:25:16 +0200

    improved handling of sequences of terminals in regex

--- src/translators/regex/regex_transform.jl
+++ src/translators/regex/regex_transform.jl
@@ -1,14 +1,11 @@
 function transform_regex_ast(ast::ASTNode)
   # flatten :or nodes with only child
   process_regex_or_nodes(ast)
-  # flatten :and nodes with only child
+  # flatten :and nodes with only child, and concatenate adjacent terminal children
   process_regex_and_nodes(ast)
   # add start node as reference to root to enable reachability check
   push!(ast.refs, ast.children[1])
 end
-
-# TODO concatenate terminals under :and
-
 
 function process_regex_or_nodes(parentnode::ASTNode)
   for idx in 1:length(parentnode.children)
@@ -28,8 +25,18 @@ function process_regex_and_nodes(parentnode::ASTNode)
     node = parentnode.children[idx]
     process_regex_and_nodes(node)
     if node.func == :and
+      # concatenate adjacent terminals
+      i = 1
+      while i < length(node.children)
+        if (node.children[i].func == :terminal) && (node.children[i+1].func == :terminal)
+          node.children[i].args[:value] *= node.children[i+1].args[:value]
+          deleteat!(node.children, i+1)
+        else
+          i += 1 
+        end
+      end
+      # flatten if a single child since no need for and rule
       if length(node.children) == 1
-        # flatten if a single child since no need for a choice
         parentnode.children[idx] = node.children[1]
       end
     end
