8393e9c08c07934b98ebde75aee0fd0df08aed9c
Simon Poulding <simon.poulding@gmail.com>
2017-03-22 09:51:17 +0100

    removed examples (now in README)

--- examples/juliacon2016/04_mathml.jl
+++
@@ -1,7 +0,0 @@
-using DataGenerators.Translators
-
-translate_from_xsd("mathml2.xsd","math", "MathMLGen","mathmlgen.jl")
-
-include("mathmlgen.jl")
-
-choose(MathMLGen)
--- examples/juliacon2016/05_library_string_from_regex.jl
+++
@@ -1,8 +0,0 @@
-using DataGenerators
-
-@generator EmailAddress begin
-    generates: "an email address"
-    start() = choose(ASCIIString, "([a-z0-9]+\\.)*[a-z0-9]+@([a-z0-9]+\\.){1,2}[a-z0-9]+")
-end
-
-addr = choose(EmailAddress())
